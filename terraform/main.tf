a3de4fc965869ed3df5326d625129ee19939a727
Harshit Malang <malangharshit@gmail.com>
2026-05-30 02:03:54 +0530

    add terraform setup

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "harshit-terraform-demo-bucket-12345"
+}
