c815ab6ac7ff58741beb1c20b990529fd37c38c2
Harshit Malang <malangharshit@gmail.com>
2026-05-30 02:37:18 +0530

    add terraform ec2 deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,48 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "harshit-terraform-demo-bucket-12345"
+resource "aws_security_group" "web_sg" {
+  name = "terraform-web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-07a00cf47dbbc844c"
+  instance_type          = "t2.micro"
+  key_name               = "todo-api"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "terraform-ec2"
+  }
 }
